c832c7fa04877fd04947f9af8804355b689ead0a
vuongprogk <vuong753333@gmail.com>
2024-05-20 17:32:23 +0700

    Add click to read

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,18 @@
+let root = document.querySelector("html");
+let theme = localStorage.getItem("theme");
+root.setAttribute("data-bs-theme", theme);
+document.getElementById("darkmode").checked = theme === "dark" ? true : false;
+
 var jsonArray;
+
+// read data from json file
 function loadData(load) {
-  console.log("success");
   fetch(".././data/giathien.json")
-    .then((res) => res.json())
-    .then((json) => {
-      load(json);
-      console.log(json);
-    });
+    .then((response) => response.json())
+    .then(load);
 }
+
+// option change reload
 function changeSection() {
   let select = document.getElementById("chuong");
   let reading = document.getElementById("reading");
@@ -16,6 +21,8 @@ function changeSection() {
     reading.innerHTML += `<p>${value}</p>`;
   });
 }
+
+// Load option for reading_view.html
 function loadOption(json) {
   let select = document.getElementById("chuong");
   json.forEach((element, index) => {
@@ -26,16 +33,35 @@ function loadOption(json) {
     select.appendChild(newOption);
   });
   jsonArray = json;
+
+  // Load section from params
+  let textSearch = window.location.search;
+  if (textSearch.length !== 0) {
+    let search = new URLSearchParams(textSearch);
+    let index = search.get("chuong");
+    if (index > -1 && index < json.length) {
+      localStorage.setItem("chuong", index - 1);
+    }
+    window.history.replaceState(null, "", window.location.pathname);
+  }
+  let chuong = localStorage.getItem("chuong");
+  if (chuong !== null && chuong > 0 && chuong < json.length) {
+    select.value = chuong;
+  }
   changeSection();
 }
 function loadList(json) {
-  json.forEach((element) => {
+  json.forEach((element, index) => {
     let select = document.getElementById("chuong");
     let link = document.createElement("a");
     link.innerText = element.title;
     link.setAttribute(
       "class",
       "text-decoration-none list-group-item-action list-group-item"
+    );
+    link.setAttribute(
+      "href",
+      `.././giathien/reading_view.html` + `?chuong=${index + 1}`
     );
     select.appendChild(link);
   });
@@ -45,15 +71,20 @@ function moveSection(i) {
   let index = parseInt(select.value);
   if ((i > 0 && index < jsonArray.length - 1) || (i < 0 && index > 0)) {
     select.value = index + i;
+    localStorage.setItem("chuong", index + 1);
     changeSection();
   }
 }
+
+//load theme
 let darkmode = document.getElementById("darkmode");
 darkmode.addEventListener("change", function () {
   let root = document.querySelector("html");
   if (darkmode.checked) {
     root.setAttribute("data-bs-theme", "dark");
+    localStorage.setItem("theme", "dark");
   } else {
     root.setAttribute("data-bs-theme", "light");
+    localStorage.setItem("theme", "light");
   }
 });
